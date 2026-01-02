97f32a3305479d63cc3a6dc8994d24b37cfc090f
Bhanuprasadh Santra <bhanuprasadh@users.noreply.github.com>
2026-01-02 22:02:06 +0530

    test API

--- app/app.py
+++ app/app.py
@@ -1,13 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import uvicorn
 
 app = FastAPI()
 
-text_posts = {}
+text_posts = {1: {"title": "New Post", "content": "cool text post"}}
 
 @app.get("/posts")
 def get_all_posts():
     return text_posts
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    if id not in text_posts:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return text_posts.get(id)
 
 
 
