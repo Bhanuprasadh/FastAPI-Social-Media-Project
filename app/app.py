dddbaabedd73d0e6551e68a01038fdaad3d8033b
Bhanuprasadh Santra <bhanuprasadh@users.noreply.github.com>
2026-01-03 23:06:22 +0530

    update app.py

--- app/app.py
+++ app/app.py
@@ -23,7 +23,8 @@ def get_post(id: int):
     return text_posts.get(id)
 
 @app.post("/posts")
-def create_post(post: PostCreate):
+def create_post(post: PostCreate) -> PostResponse:
     new_post = {"title": post.title, "content": post.content}
     text_posts[max(text_posts.keys()) + 1] = new_post
     return new_post
+
--- app/schemas.py
+++ app/schemas.py
@@ -2,4 +2,8 @@ from pydantic import BaseModel
 
 class PostCreate(BaseModel):
     title: str
+    content: str
+
+class PostResponse(BaseModel):
+    title: str
     content: str 
