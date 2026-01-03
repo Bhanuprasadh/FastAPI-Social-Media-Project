f8d89affe4fb0b00e21cdaafb2cbbd90bcb281f8
Bhanuprasadh Santra <bhanuprasadh@users.noreply.github.com>
2026-01-03 23:00:25 +0530

    update app.py

--- app/app.py
+++ app/app.py
@@ -24,4 +24,6 @@ def get_post(id: int):
 
 @app.post("/posts")
 def create_post(post: PostCreate):
-    text_posts[max(text_posts.keys()) + 1] = {"title": post.title, "content": post.content}
+    new_post = {"title": post.title, "content": post.content}
+    text_posts[max(text_posts.keys()) + 1] = new_post
+    return new_post
