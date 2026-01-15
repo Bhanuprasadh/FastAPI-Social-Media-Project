aead64cc9efc26b79268554fb2b5994e000d8f7f
Bhanuprasadh Santra <bhanuprasadh@users.noreply.github.com>
2026-01-15 20:32:32 +0530

    update fastapi template

--- app/app.py
+++ app/app.py
@@ -6,6 +6,8 @@ text_post = {1: {"Name": "Rupa", "Gender": "Female"}}
 
 @app.get("/posts")
 def get_all_posts(limit: int=None):
+    if limit:
+        return list(text_post.values())[:limit]
     return text_post
 
 @app.get("/posts/{id}")
