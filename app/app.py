01039389f327c541f6b42ba3a26badccfa969224
Bhanuprasadh Santra <bhanuprasadh@users.noreply.github.com>
2026-01-15 20:40:41 +0530

    Add Schema

--- app/app.py
+++ app/app.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-text_post = {1: {"Name": "Rupa", "Gender": "Female"}}
+text_post = {1: {"Name": "Rupa", "Gender": "Female"},
+             2: {"Name": "Bhanuprasadh", "Gender": "Male"}}
 
 @app.get("/posts")
 def get_all_posts(limit: int=None):
@@ -15,3 +16,8 @@ def get_post(id: int):
     if id not in text_post:
         raise HTTPException(status_code=404, detail="Post not found")
     return text_post.get(id)
+
+
+@app.post("/posts")
+def create_post():
+    pass
